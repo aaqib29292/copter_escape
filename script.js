cdfa896ef43bd99735d9fad7cedd21293b3d483e
Aaqib Parvez <aaqib29292@gmail.com>
2016-10-25 00:59:33 +0530

    made moving copter for the game

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ var ctx = c.getContext("2d");
 var bgImage = new Image();
 bgImage.src = "images/bg.png";
 
+var copterImg = new Image();
+copterImg.src = "images/copter.png";
+
+
 function update() {
   updateBackground();
+  updateCopter();
 
   requestAnimationFrame(update)
 }
 
 function draw() {
   drawBackground();
+  drawCopter();
 
   requestAnimationFrame(draw);
 }
@@ -37,4 +43,25 @@ function drawBackground() {
   ctx.drawImage(bgImage, bgX, bgY);
   ctx.drawImage(bgImage, bgX +856, bgY);
 
+}
+
+// Copter
+
+var copterX = 30;
+var copterY = 200;
+
+var copterWidth = 131;
+var copterheight = 34;
+
+var frameIndex = 0;
+
+function updateCopter() {
+  frameIndex++;
+  frameIndex = frameIndex%3;
+}
+
+function drawCopter() {
+  ctx.drawImage(copterImg, copterWidth*frameIndex, 0,
+                 copterWidth, copterheight, copterX, copterY,
+                  copterWidth, copterheight);
 }
