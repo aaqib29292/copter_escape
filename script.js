42b79811d9fe32261d111d2761769295de422036
Aaqib Parvez <aaqib29292@gmail.com>
2016-10-25 00:30:27 +0530

    adding background of the game

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+console.log("hi");
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+
+var bgImage = new Image();
+bgImage.src = "images/bg.png";
+
+function draw() {
+  drawBackground();
+
+  requestAnimationFrame(draw);
+}
+
+draw();
+
+function drawBackground() {
+  ctx.drawImage(bgImage, 0, 0);
+}
