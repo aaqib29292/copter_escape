a77b3bea02066b1f178d521f1451f535ac86b99a
Aaqib Parvez <aaqib29292@gmail.com>
2016-10-25 00:40:47 +0530

    made moving background of the game

--- script.js
+++ script.js
@@ -7,14 +7,34 @@ var ctx = c.getContext("2d");
 var bgImage = new Image();
 bgImage.src = "images/bg.png";
 
+function update() {
+  updateBackground();
+
+  requestAnimationFrame(update)
+}
+
 function draw() {
   drawBackground();
 
   requestAnimationFrame(draw);
 }
 
+update()
 draw();
 
+// Background
+var bgX = 0;
+var bgY = 0;
+
+function updateBackground() {
+  bgX = bgX - 2
+  if (bgX < -856) {
+    bgX = 0
+  }
+}
+
 function drawBackground() {
-  ctx.drawImage(bgImage, 0, 0);
+  ctx.drawImage(bgImage, bgX, bgY);
+  ctx.drawImage(bgImage, bgX +856, bgY);
+
 }
